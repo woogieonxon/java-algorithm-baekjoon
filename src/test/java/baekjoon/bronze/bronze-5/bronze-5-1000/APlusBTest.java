ff4fdbc3ec846951a907f860b8408272624c1cb0
woogie <woogieonxon@gmail.com>
2025-10-22 22:15:28 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ src/test/java/baekjoon/bronze/bronze-5/bronze-5-1000/APlusBTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class APlusBTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A+B);
+
+        scanner.close();
+    }
+}
