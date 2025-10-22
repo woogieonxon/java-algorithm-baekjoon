b2906af0399b55bb4b46a6f3ae52f6a1def254db
woogie <woogieonxon@gmail.com>
2025-10-22 23:20:49 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

--- src/main/java/baekjoon/bronze/bronze-5/bronze-5-1000/AddTwoNumbers.java
+++ src/main/java/baekjoon/bronze/bronze-5/bronze-5-1000/AddTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class APlusB {
+public class AddTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
--- src/test/java/baekjoon/bronze/bronze-5/bronze-5-1000/AddTwoNumbersTest.java
+++ src/test/java/baekjoon/bronze/bronze-5/bronze-5-1000/AddTwoNumbersTest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class APlusBTest {
+public class AddTwoNumbersTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
